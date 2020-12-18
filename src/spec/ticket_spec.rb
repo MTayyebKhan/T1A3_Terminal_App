e070914f6d3403252af38d454888bd5f17a2a83b
MTayyebKhan <tayyeb.10@live.com>
2020-12-18 22:50:26 +1000

    shell script works, deleted the folder, it created on test, added comment to testing

--- src/spec/ticket_spec.rb
+++ src/spec/ticket_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../classes/ticket.rb'
 
+# Rspec Test
 describe Ticket do
     it "to_s method should return a String" do
         ticket = Ticket.new("test_name", "test_event", "test_location", "test_status")
