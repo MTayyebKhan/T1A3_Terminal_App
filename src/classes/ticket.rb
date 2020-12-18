7edbcfbd86e80174a6e7787a94f42f4cb25a8f33
MTayyebKhan <tayyeb.10@live.com>
2020-12-18 21:52:02 +1000

    added extra folders, created main rb and ticket class

---
+++ src/classes/ticket.rb
@@ -0,0 +1,20 @@
+class Ticket
+    attr_reader :name, :event, :location, :vip
+    attr_writer :name, :event, :location, :vip
+    def initialize(name, event, location, vip)
+      @name = name
+      @event = event
+      @location = location
+      @vip = vip
+    end
+    def to_s
+      receipt = ""
+      receipt << "************************\n"
+      receipt << "Name:  ---------- #{@name}\n\n"
+      receipt << "Event: ---------- #{@event}\n\n"
+      receipt << "Location: ------- #{@location}\n\n"
+      receipt << "VIP: ------------ #{@vip}\n"
+      receipt << "************************"
+      return receipt
+    end
+end
---
+++ src/main.rb
@@ -0,0 +1,20 @@
+require_relative "./classes/ticket.rb"
+require_relative "./methods/methods.rb"
+
+if ARGV.length > 0
+    username = ARGV[0]
+    ARGV.clear
+else
+    puts "What is your username?"
+    username = gets.chomp
+end
+puts "Welcome #{username} to Event Tickets."
+sleep(1.5)
+
+begin
+    num 10/0
+rescue ZeroDivisionError
+    puts "Division by zero error"
+end
+
+main
