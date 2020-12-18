74416b7a261a3c6239da8029418ea245ebe33a09
MTayyebKhan <tayyeb.10@live.com>
2020-12-18 22:26:29 +1000

    created a shell script to install and run from git

---
+++ src/shell.sh
@@ -0,0 +1,8 @@
+echo "Thanks for using Event Tickets."
+echo "Installation in progress"
+git clone "https://github.com/MTayyebKhan/T1A3_Terminal_App.git"
+cd T1A3/src
+echo "Bundling install files"
+bundle install
+echo "Bundle complete. Running program."
+ruby main.rb
